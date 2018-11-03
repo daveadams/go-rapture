171f53266c78c11fbf61b9cd1a3cc834f32c2766
David Adams <daveadams@gmail.com>
2018-11-03 08:02:49 -0500

    Bump to v2.0.0-alpha3

--- cmd/rapture/command_version.go
+++ cmd/rapture/command_version.go
@@ -1,6 +1,6 @@
 package main
 
-var RaptureVersion = "2.0.0-alpha2"
+var RaptureVersion = "2.0.0-alpha3"
 
 func CommandVersion(cmd string, args []string) int {
 	shgen.Echof("rapture v%s", RaptureVersion)
